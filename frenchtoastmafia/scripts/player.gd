8c365741f33ef757507d5f5c994b9753d14a61c3
rohrsa <rohrsa@sonoma.edu>
2025-02-24 21:24:23 -0800

    add double jump

--- frenchtoastmafia/scripts/player.gd
+++ frenchtoastmafia/scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+var canDoubleJump: bool
 	
 
 func _physics_process(delta: float) -> void:		
@@ -11,8 +12,14 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		canDoubleJump = true
 		velocity.y = JUMP_VELOCITY
 
+	#double jump
+	if canDoubleJump and Input.is_action_just_pressed("jump") and not is_on_floor():
+		canDoubleJump = false
+		velocity.y = JUMP_VELOCITY
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
