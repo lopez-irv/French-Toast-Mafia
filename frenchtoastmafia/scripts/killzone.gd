e64e0cc8e9e2dc877efa323334f70b6939a6ffb3
rohrsa <rohrsa@sonoma.edu>
2025-02-26 21:51:09 -0800

    add level timer

--- frenchtoastmafia/scripts/killzone.gd
+++ frenchtoastmafia/scripts/killzone.gd
@@ -10,7 +10,7 @@ func _on_body_entered(body: Node2D) -> void:
 	timer.start() 
 
 
-#restarts game once time runs out
+#restarts game after short pause
 func _on_timer_timeout() -> void:
 	#Engine.time_scale = 1.0	 #set time back to normal
 	get_tree().change_scene_to_file("res://scenes/game_over.tscn")
