94480bee4a76005d0c89d03c9a958ca951bcd003
Irving Lopez <irvinglopez503@gmail.com>
2025-03-06 08:30:36 -0800

    resolve conlficts

--- frenchtoastmafia/scripts/slime.gd
+++ frenchtoastmafia/scripts/slime.gd
@@ -8,6 +8,9 @@ var player = null
 @onready var ray_cast_left: RayCast2D = $rayCastLeft
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+# 3D slime sound player node (AudioStreamPlayer3D)
+@onready var slime_sound: AudioStreamPlayer2D = $SlimeSoundPlayer
+var slime_sound_stream = preload("res://assets/sounds/slime-sound.mp3")
 
 #@onready var players = get_tree().get_nodes_in_group("Player")
 
@@ -17,7 +20,10 @@ func _ready() -> void:
 	var players = get_tree().get_nodes_in_group("Player")
 	if players.size() > 0:
 		player = players[0]  # Assign the first player found.
-
+		
+	# Set the slime sound stream to the 3D audio player.
+	slime_sound.stream = slime_sound_stream
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
@@ -30,7 +36,9 @@ func _process(delta: float) -> void:
 		animated_sprite.flip_h = false
 	position.x += direction * SPEED * delta
 	
-
+	if can_play:
+		play_slime_sound()
+		
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("hit player")
 	if body.is_in_group("Player"):
@@ -39,3 +47,10 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 			#var healthBar = body.get_node("healthBar") as ProgressBar
 			#healthBar.value = player.health  # update health bar visually
 		
+var can_play = true
+func play_slime_sound():
+	if can_play:
+		slime_sound.play()
+		can_play = false
+		await get_tree().create_timer(0.30).timeout  # Delay before allowing the sound to play again.
+		can_play = true
