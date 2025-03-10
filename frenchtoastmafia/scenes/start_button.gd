6b0aa3ce3ca6543a0621fe90f0917f1392487c54
Irving Lopez <irvinglopez503@gmail.com>
2025-03-10 14:16:38 -0700

    remove start game button and keep only level selection, on game over scene remove restart game button and put in level selection, add inventory to level1

--- frenchtoastmafia/scenes/start_button.gd
+++ frenchtoastmafia/scenes/start_button.gd
@@ -8,4 +8,5 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
+	print(get_tree().root.name)
 	get_tree().change_scene_to_file("res://scenes/game.tscn") 
---
+++ frenchtoastmafia/scripts/level_selection.gd
@@ -0,0 +1,14 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	connect("pressed", Callable(self, "_on_level_selection_pressed"))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_level_selection_pressed():
+	get_tree().change_scene_to_file("res://scenes/levelSelection.tscn") 
