a08dcefa339926703b9ac51b713fa90aa8e28ed3
Cristian Villalobos <Cristian.villalobos.5244@gmail.com>
2025-03-09 17:13:33 -0700

    level selection added

---
+++ frenchtoastmafia/scripts/levelSelectionButton0.gd
@@ -0,0 +1,11 @@
+extends Button
+
+func _ready():
+	connect("pressed", _on_start_button_pressed) 
+
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn") 
