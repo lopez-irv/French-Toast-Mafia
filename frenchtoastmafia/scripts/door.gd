a89f4b10c4c7de10f5c36131117ae63051dbdc3a
Irving Lopez <irvinglopez503@gmail.com>
2025-03-23 18:30:25 -0700

    coin in HUD, coin count persist across levels

--- frenchtoastmafia/scripts/door.gd
+++ frenchtoastmafia/scripts/door.gd
@@ -7,7 +7,9 @@ func _input(event):      #KEY TO ENTER DOORS IS "ENTER/RETURN"
 	if event.is_action_pressed("ui_accept"): 
 		if get_overlapping_bodies().size() > 0: 
 			level_unlock_status.level1 = true	#CHANGE LATER TO WORK W ALL SCENES
+			CoinGlobal._commit_coins()
 			next_level()
+
 func next_level():                 #CHANGE ME LATER TO WORK FOR ALL SCENES
 	var ERR = get_tree().change_scene_to_file("res://scenes/levelSelection.tscn")
 	
