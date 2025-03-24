44459d6e1b81a523e799cd9b27d920ec07692528
Irving Lopez <irvinglopez503@gmail.com>
2025-03-24 16:28:14 -0700

    add more to the shop scene, connect the shop to the level selection

---
+++ frenchtoastmafia/scenes/shop_selection_button.gd
@@ -0,0 +1,12 @@
+extends Button
+
+func _ready():
+	connect("pressed", _on_start_button_pressed) 
+
+
+func _process(delta: float) -> void:
+	#pass
+	set("theme_override_colors/font_color",Color(0,1,0))
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/shop.tscn") 
