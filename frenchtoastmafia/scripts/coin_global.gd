1595cb4b42b266535f5220d65c7d9341cbf8c374
rohrsa <rohrsa@sonoma.edu>
2025-04-25 11:47:13 -0700

    add red coins to level 3 and started on function to unlock skin once all are found

--- frenchtoastmafia/scripts/coin_global.gd
+++ frenchtoastmafia/scripts/coin_global.gd
@@ -2,6 +2,10 @@ extends Node
 
 var total_coins: int
 var coins_this_level: int 
+
+var red_coins_collected: int = 0
+var secret_skin_unlocked: bool = false #unlocks after all red coins are found
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
