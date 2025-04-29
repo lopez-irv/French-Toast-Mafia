19de7ba17603b722574192f72e1be04855ce0472
Cristian Villalobos <Cristian.villalobos.5244@gmail.com>
2025-04-28 22:04:35 -0700

    start of boss level, added animations to boss goblin, final boss level is called boss_level.tscn

---
+++ frenchtoastmafia/scripts/goblin_boss.gd
@@ -0,0 +1,12 @@
+extends Node2D
+@onready var sprite := $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	sprite.play("attack1")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
