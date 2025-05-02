699f875196ad27f29292b9185c073f785d828ace
rohrsa <rohrsa@sonoma.edu>
2025-05-02 13:01:49 -0700

    Made it possible to use arrow keys on menu and level selection

--- frenchtoastmafia/scenes/level_selection.gd
+++ frenchtoastmafia/scenes/level_selection.gd
@@ -5,6 +5,7 @@ var level_select_music = preload("res://assets/music/level-select-music.wav")
 @onready var level_music_player: AudioStreamPlayer2D = $LevelMusicPlayer
 
 func _ready() -> void:
+	$MarginContainer/CenterContainer/VBoxContainer/row1/level0.grab_focus()
 	player_level_global.health = player_level_global.healthCap
 	level_music_player.stream = level_select_music
 	level_music_player.play()
--- frenchtoastmafia/scenes/main_menu/main_menu.gd
+++ frenchtoastmafia/scenes/main_menu/main_menu.gd
@@ -5,11 +5,13 @@ var main_menu_music = preload("res://assets/music/main-menu-music.wav")
 @onready var menu_music_player: AudioStreamPlayer2D = $MenuMusicPlayer
 
 func _ready() -> void:
+	$MarginContainer/HBoxContainer/VBoxContainer/level_selection.grab_focus()
 	menu_music_player.stream = main_menu_music
 	menu_music_player.play()
 
 func _process(delta: float) -> void:
 	pass
+
 
 func _on_levels_button_pressed() -> void:
 	# Change scene when the levels button is pressed.
