a260cbaa0722db03cd43b5a498718270a10d0485
rohrsa <rohrsa@sonoma.edu>
2025-05-04 21:29:32 -0700

    Add button for secret skin option to skin menu which is hidden unless all red coins have been found

---
+++ frenchtoastmafia/scripts/secret.gd
@@ -0,0 +1,14 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if !CoinGlobal.secret_skin_unlocked:
+		hide()
+	else:
+		show()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
